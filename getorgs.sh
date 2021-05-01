352fe451702086c7ce3fda94e50f1234ec174cd7
Pramodh M <54981492+pramodhm112@users.noreply.github.com>
2021-05-01 23:27:25 +0530

    Update getorgs.sh

--- getorgs.sh
+++ getorgs.sh
@@ -10,27 +10,26 @@ then
         apiendpoint=$(echo ${apiendpoint::-1})
 fi
 
-linkheader="true"
-
-curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=5" | jq .[].login | tr -d '"'
-
+curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | jq .[].login | tr -d '"'
 link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | awk -F " " '/Link:/ {print $2}' | tr -d "<>;")
-
 echo $link >> nextorglinks
 
-while [[ $linkheader=true ]]
+if [[ $link = ${apiendpoint}/organizations{?since} ]]
+then
+        break
+fi
+
+while [[ $link != ${apiendpoint}/organizations{?since} ]]
 do
+        if [[ $link != ${apiendpoint}/organizations{?since} ]]
+        then
         link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$link" | awk -F " " '/Link:/ {print $2}' | tr -d "<>;")
         echo $link >> nextorglinks
-        if [[ $link=${apiendpoint}/organizations{?since} ]]
-        then
-                break
         fi
 done
 
+sed -i '$d' nextorglinks
 while read line || [ -n "$line" ]
 do
         curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$line" | jq .[].login | tr -d '"'
 done < "nextorglinks"
-
-rm nextorglinks > /dev/null 2>&1
