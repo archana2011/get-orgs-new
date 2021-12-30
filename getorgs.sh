927d2c273342dcc8adb5a67c89fb2ebfc284ece3
Pramodh M <54981492+pramodhm112@users.noreply.github.com>
2021-12-30 06:29:40 +0530

    Update getorgs.sh

--- getorgs.sh
+++ getorgs.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 rm nextorglinks > /dev/null 2>&1
-export token=$OCTOKIT_ACCESS_TOKEN
+rm orgnames > /dev/null 2>&1
+
+export token=GITHUB_TOKEN
 export pagelen=100
-export apiendpoint=$OCTOKIT_API_ENDPOINT
+export apiendpoint=HTTPS_SERVER_URL
 
 if [[ $(echo "${apiendpoint: -1}") = / ]]
 then
         apiendpoint=$(echo ${apiendpoint::-1})
 fi
 
-curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | jq .[].login | tr -d '"'
-link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | awk -F " " '/Link:/ {print $2}' | tr -d "<>;")
+curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | jq .[].login | tr -d '"' >> orgnames.csv
+link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "${apiendpoint}/organizations?per_page=${pagelen}" | awk -F " " '/link:/ {print $2}' | tr -d "<>;")
 echo $link >> nextorglinks
 
 if [[ $link = ${apiendpoint}/organizations{?since} ]]
@@ -23,7 +25,7 @@ while [[ $link != ${apiendpoint}/organizations{?since} ]]
 do
         if [[ $link != ${apiendpoint}/organizations{?since} ]]
         then
-        link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$link" | awk -F " " '/Link:/ {print $2}' | tr -d "<>;")
+        link=$(curl -i -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$link" | awk -F " " '/link:/ {print $2}' | tr -d "<>;")
         echo $link >> nextorglinks
         fi
 done
@@ -31,5 +33,8 @@ done
 sed -i '$d' nextorglinks
 while read line || [ -n "$line" ]
 do
-        curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$line" | jq .[].login | tr -d '"'
+        curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $token" -X GET "$line" | jq .[].login | tr -d '"' >> orgnames.csv
 done < "nextorglinks"
+
+rm nextorglinks > /dev/null 2>&1
+sed -i 's/\r//g' orgnames.csv
